6bc4938caeb017a95e25772670dcbd4d21b615c1
ilkawntr <winterilka@gmail.com>
2019-11-06 10:19:51 +0100

    renaming and listBlog

--- app/src/main/java/com/example/weitblickapp_android/ui/news/NewsFragment.java
+++ app/src/main/java/com/example/weitblickapp_android/ui/news/NewsFragment.java
@@ -4,20 +4,30 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import com.example.weitblickapp_android.R;
-
+import android.widget.TextView;
+import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import com.example.weitblickapp_android.R;
 
 public class NewsFragment extends Fragment {
 
-    View view;
+    private NewsViewModel newsViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
-            ViewGroup container, Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.fragment_news, container, false);
-        return view;
+                             ViewGroup container, Bundle savedInstanceState) {
+        newsViewModel =
+                ViewModelProviders.of(this).get(NewsViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_news, container, false);
+        final TextView textView = root.findViewById(R.id.text_news);
+        newsViewModel.getText().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
     }
 }
