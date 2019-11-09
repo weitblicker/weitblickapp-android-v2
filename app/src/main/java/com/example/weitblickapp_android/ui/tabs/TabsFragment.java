ed357b2018799942898a34ffb123124157b83b69
ilkawntr <winterilka@gmail.com>
2019-11-09 18:11:46 +0100

    Tab fix and App name

--- app/src/main/java/com/example/weitblickapp_android/ui/tabs/TabsFragment.java
+++ app/src/main/java/com/example/weitblickapp_android/ui/tabs/TabsFragment.java
@@ -5,18 +5,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProviders;
-import androidx.viewpager.widget.PagerAdapter;
-import androidx.viewpager.widget.ViewPager;
-
 import com.example.weitblickapp_android.R;
 import com.example.weitblickapp_android.ui.ViewPageAdapter;
-import com.example.weitblickapp_android.ui.news.NewsFragment;
-import com.example.weitblickapp_android.ui.news.NewsViewModel;
 import com.example.weitblickapp_android.ui.event.EventFragment;
+import com.example.weitblickapp_android.ui.news.NewsFragment;
 import com.google.android.material.tabs.TabLayout;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 public class TabsFragment extends Fragment {
 
@@ -31,7 +28,7 @@ public class TabsFragment extends Fragment {
         tabLayout = (TabLayout) root.findViewById(R.id.tab_layout);
         viewPager = (ViewPager) root.findViewById(R.id.pager);
 
-        ViewPageAdapter adapter = new ViewPageAdapter(getFragmentManager());
+        ViewPageAdapter adapter = new ViewPageAdapter(getChildFragmentManager());
         adapter.AddFragment(new NewsFragment(), "Neuheiten");
         adapter.AddFragment(new EventFragment(), "Veranstaltungen");
 
