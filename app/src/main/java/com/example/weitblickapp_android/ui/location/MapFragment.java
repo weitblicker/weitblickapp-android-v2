9aa7ad3e88b7e3da747db159447890446df5de80
ilkawntr <winterilka@gmail.com>
2019-12-04 14:24:35 +0100

    distance + location

--- app/src/main/java/com/example/weitblickapp_android/ui/location/MapFragment.java
+++ app/src/main/java/com/example/weitblickapp_android/ui/location/MapFragment.java
@@ -6,11 +6,16 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.weitblickapp_android.MainActivity;
 import com.example.weitblickapp_android.R;
 import com.example.weitblickapp_android.data.Session.SessionManager;
 import com.example.weitblickapp_android.ui.ranking.RankingFragment;
@@ -32,11 +37,21 @@ import androidx.fragment.app.FragmentTransaction;
 public class MapFragment extends Fragment implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-    Location currentLocation;
+    private Location currentLocation;
+    private Location lastLocation;
     private SessionManager session;
-    boolean paused = false;
+    private boolean paused = false;
+    private boolean load = false;
+    static private double km = 0;
+    private Handler handler = new Handler();
+    private int delay = 1000; //milliseconds
+    private TextView distance;
+    private TextView speedKmh;
+    private TextView donation;
+    private double betrag = 0.10;
+    static private double don = 0;
 
-    FusedLocationProviderClient fusedLocationProviderClient;
+    private FusedLocationProviderClient fusedLocationProviderClient;
     private static final int REQUEST_CODE = 101;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -47,39 +62,48 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
 
         final ImageView pause = root.findViewById(R.id.pause);
         ImageView stop = root.findViewById(R.id.stop);
+        distance = root.findViewById(R.id.distance);
+        speedKmh = root.findViewById(R.id.speed);
+        donation = root.findViewById(R.id.donation);
 
+        if(getActivity() != null)
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getActivity());
+        fetchLastLocation();
+        startFetchLocation();
         pause.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(!paused){
                     pause.setImageResource(R.mipmap.ic_play_foreground);
                     paused=true;
+                    delay = 0;
                 }else{
                     pause.setImageResource(R.mipmap.ic_pause);
                     paused=false;
+                    delay = 1000;
+                    startFetchLocation();
                 }
             }
         });
-
         stop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 EndFragment fragment = new EndFragment();
-                FragmentTransaction ft = getActivity().getSupportFragmentManager().beginTransaction();
+                FragmentTransaction ft = getChildFragmentManager().beginTransaction();
                 ft.replace(R.id.fragment_container, fragment);
+                delay = 0;
                 ft.commit();
             }
         });
-
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getActivity());
-        fetchLastLocation();
         return root;
     }
 
     private void fetchLastLocation(){
-        if(ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
-            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
-            return;
+        if(getContext() != null  && getActivity() != null){
+            if(ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+                ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+                return;
+            }
         }
         Task<Location> task = fusedLocationProviderClient.getLastLocation();
         task.addOnSuccessListener(new OnSuccessListener<Location>() {
@@ -87,13 +111,25 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
             public void onSuccess(Location location) {
                 if(location != null){
                     currentLocation = location;
-                    setUpMapIfNeeded();
+                    if(!load) {
+                        setUpMapIfNeeded();
+                    }
                 }
             }
         });
-
+        checkKm();
     }
 
+    private void startFetchLocation(){
+        handler.postDelayed(new Runnable(){
+            public void run(){
+                fetchLastLocation();
+                if(delay != 0){
+                    handler.postDelayed(this, delay);
+                }
+            }
+        }, delay);
+    }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
@@ -108,6 +144,19 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         }
     }
 
+    private void checkKm(){
+        if(lastLocation != null){
+            double dis = currentLocation.distanceTo(lastLocation) / 1000;
+            double speed = currentLocation.getSpeed();
+            km += dis;
+            don = (betrag * km) / 100;
+            speedKmh.setText((String.valueOf(Math.round(speed * 10.00) / 10.00)) + "km/h");
+            distance.setText((String.valueOf(Math.round(km * 100.00) / 100.00)) + " km");
+            donation.setText((String.valueOf(Math.round(don * 100.00) / 100.00)) + " €");
+        }
+        lastLocation = currentLocation;
+    }
+
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permission, @NonNull int[] grantResult){
         switch (requestCode){
@@ -116,13 +165,15 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
                     fetchLastLocation();
                 }
                 break;
+            default:
+                break;
         }
     }
-
 
     private void setUpMapIfNeeded() {
         SupportMapFragment mapFrag = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
         mapFrag.getMapAsync(this);
+        load = true;
     }
 
     private boolean isLocationEnabled(){
