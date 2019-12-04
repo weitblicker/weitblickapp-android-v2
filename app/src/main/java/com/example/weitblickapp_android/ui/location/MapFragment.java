03239aa081556aaacc9608894d0d5713d4ccc9dc
jpunke <j.punke@yahoo.de>
2019-12-04 14:25:15 +0100

    Add Post-Request for Segments

--- app/src/main/java/com/example/weitblickapp_android/ui/location/MapFragment.java
+++ app/src/main/java/com/example/weitblickapp_android/ui/location/MapFragment.java
@@ -6,14 +6,27 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.weitblickapp_android.R;
 import com.example.weitblickapp_android.data.Session.SessionManager;
-import com.example.weitblickapp_android.ui.ranking.RankingFragment;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -24,10 +37,11 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 
-import androidx.annotation.NonNull;
-import androidx.core.app.ActivityCompat;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MapFragment extends Fragment implements OnMapReadyCallback {
 
@@ -68,6 +82,7 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
                 FragmentTransaction ft = getActivity().getSupportFragmentManager().beginTransaction();
                 ft.replace(R.id.fragment_container, fragment);
                 ft.commit();
+                sendSegment();
             }
         });
 
@@ -75,6 +90,49 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         fetchLastLocation();
         return root;
     }
+
+    private void sendSegment(){
+
+        String URL = "new.weitblicker.org/rest/cycle/segment";
+
+        JSONObject jsonBody = new JSONObject();
+        try {
+            jsonBody.put("start", "2019-10-01T07:08");
+            jsonBody.put("end", "2019-10-01T07:08");
+            jsonBody.put("distance", "0.5");
+            jsonBody.put("tour", "1");
+        }catch(JSONException e) {
+        }
+        RequestQueue requestQueue = Volley.newRequestQueue(getActivity().getApplicationContext());
+        JsonObjectRequest objectRequest = new JsonObjectRequest(Request.Method.POST, URL, jsonBody, new Response.Listener<JSONObject>() {
+
+            @Override
+            public void onResponse(JSONObject response) {
+
+            }
+
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //Display Error Message
+                Log.e("Rest Response", error.toString());
+            }
+        }){
+            //Override getHeaders() to set Credentials for REST-Authentication
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> headers = new HashMap<>();
+                String credentials = "surfer:hangloose";
+                String auth = "Basic "
+                        + Base64.encodeToString(credentials.getBytes(), Base64.NO_WRAP);
+                headers.put("Content-Type", "application/json");
+                headers.put("Authorization", auth);
+                return headers;
+            }
+        };
+        requestQueue.add(objectRequest);
+    }
+
 
     private void fetchLastLocation(){
         if(ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
