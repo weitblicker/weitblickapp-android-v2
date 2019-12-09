8daac35e09ea1fdae303bec7d5b13da487995aad
jpunke <j.punke@yahoo.de>
2019-12-09 23:08:13 +0100

    Add ranking-Model & get Request for rankings

--- app/src/main/java/com/example/weitblickapp_android/ui/ranking/RankingFragment.java
+++ app/src/main/java/com/example/weitblickapp_android/ui/ranking/RankingFragment.java
@@ -1,19 +1,41 @@
 package com.example.weitblickapp_android.ui.ranking;
 
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.weitblickapp_android.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class RankingFragment extends Fragment {
 
     private RankingViewModel rankingViewModel;
+    private ArrayList<RankingViewModel> rankings = new ArrayList<>();
+
+    static final String url = "https://new.weitblicker.org/rest/cycle/ranking/";
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -22,5 +44,67 @@ public class RankingFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_ranking, container, false);
 
         return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        getRankingData();
+    }
+
+    public void getRankingData(){
+
+        RequestQueue requestQueue = Volley.newRequestQueue(getActivity().getApplicationContext());
+
+        JsonArrayRequest objectRequest = new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+
+            @Override
+            public void onResponse(JSONArray response) {
+                //Save Data into Model
+                String jsonData = response.toString();
+                Log.e("RANKING:", jsonData);
+
+                //Parse the JSON response array by iterating over it
+                for (int i = 0; i < response.length(); i++) {
+                    JSONObject responseObject = null;
+
+                    try {
+                        responseObject = response.getJSONObject(i);
+
+                        String username = responseObject.getString("username");
+                        String imageUrl = responseObject.getString("image");
+                        double distance = responseObject.getDouble("km");
+                        double donation = responseObject.getDouble("euro");
+
+                        RankingViewModel temp = new RankingViewModel(imageUrl, username, distance, donation);
+                        rankings.add(temp);
+                        //adapter.notifyDataSetChanged();
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //Display Error Message
+                Log.e("Ranking ErrorResponse", error.toString());
+            }
+        }){
+            //Override getHeaders() to set Credentials for REST-Authentication
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> headers = new HashMap<>();
+                String credentials = "surfer:hangloose";
+                String auth = "Basic "
+                        + Base64.encodeToString(credentials.getBytes(), Base64.NO_WRAP);
+                headers.put("Content-Type", "application/json");
+                headers.put("Authorization", auth);
+                return headers;
+            }
+        };
+        requestQueue.add(objectRequest);
     }
 }
--- app/src/main/java/com/example/weitblickapp_android/ui/ranking/RankingViewModel.java
+++ app/src/main/java/com/example/weitblickapp_android/ui/ranking/RankingViewModel.java
@@ -1,19 +1,55 @@
 package com.example.weitblickapp_android.ui.ranking;
 
-import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class RankingViewModel extends ViewModel {
 
-    private MutableLiveData<String> mText;
+    private String profileImageUrl;
+    private String username;
+    private double cycledKm;
+    private double cycledDonation;
+
 
     public RankingViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("Hier werden bald Rankings stehen!");
+
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public RankingViewModel(String profileImageUrl, String username, double cycledKm, double cycledDonation) {
+        this.profileImageUrl = profileImageUrl;
+        this.username = username;
+        this.cycledKm = cycledKm;
+        this.cycledDonation = cycledDonation;
+    }
+
+    public String getProfileImageUrl() {
+        return profileImageUrl;
+    }
+
+    public void setProfileImageUrl(String profileImageUrl) {
+        this.profileImageUrl = profileImageUrl;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public double getCycledKm() {
+        return cycledKm;
+    }
+
+    public void setCycledKm(double cycledKm) {
+        this.cycledKm = cycledKm;
+    }
+
+    public double getCycledDonation() {
+        return cycledDonation;
+    }
+
+    public void setCycledDonation(double cycledDonation) {
+        this.cycledDonation = cycledDonation;
     }
 }
