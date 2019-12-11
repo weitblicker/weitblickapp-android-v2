961c8a5465df523dc00f2eed71ca969e3418f87c
ilkawntr <winterilka@gmail.com>
2019-12-11 12:10:46 +0100

    RankingList

--- app/src/main/java/com/example/weitblickapp_android/ui/ranking/RankingFragment.java
+++ app/src/main/java/com/example/weitblickapp_android/ui/ranking/RankingFragment.java
@@ -1,26 +1,121 @@
 package com.example.weitblickapp_android.ui.ranking;
 
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
 import android.widget.ListView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.ListFragment;
 import androidx.lifecycle.ViewModelProviders;
-import com.example.weitblickapp_android.R;
 
-public class RankingFragment extends Fragment {
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.weitblickapp_android.R;
+import com.example.weitblickapp_android.ui.news.NewsListAdapter;
+import com.example.weitblickapp_android.ui.news.NewsViewModel;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RankingFragment extends ListFragment{
 
     private RankingViewModel rankingViewModel;
+    private RankingListAdapter adapter;
+    ArrayList<RankingViewModel> rankings = new ArrayList<RankingViewModel>();
+    private String url = "https://new.weitblicker.org/rest/cycle/ranking/";
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        rankingViewModel =
-                ViewModelProviders.of(this).get(RankingViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_ranking, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        return root;
+        View view = inflater.inflate(R.layout.fragment_ranking, container, false);
+
+        adapter = new RankingListAdapter(getActivity(), rankings, getFragmentManager());
+        this.setListAdapter(adapter);
+        ListView listView = (ListView) view.findViewById(R.id.list);
+
+        return view;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        loadNews(url);
+    }
+
+
+    private void loadNews(String URL){
+
+        // Talk to Rest API
+
+        RequestQueue requestQueue = Volley.newRequestQueue(getActivity().getApplicationContext());
+
+        JsonArrayRequest objectRequest = new JsonArrayRequest(Request.Method.GET, URL, null, new Response.Listener<JSONArray>() {
+
+            @Override
+            public void onResponse(JSONArray response) {
+                //Save Data into Model
+                String jsonData = response.toString();
+
+                //Parse the JSON response array by iterating over it
+                for (int i = 0; i < response.length(); i++) {
+                    JSONObject responseObject = null;
+
+                    try {
+                        responseObject = response.getJSONObject(i);
+                        String username = responseObject.getString("username");
+                        float km = responseObject.getLong("km");
+                        float donation = responseObject.getLong("euro");
+                        String imageProfile = responseObject.getString("image");
+
+                        RankingViewModel temp = new RankingViewModel(username, km, donation, imageProfile);
+                        rankings.add(temp);
+                        adapter.notifyDataSetChanged();
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //Display Error Message
+                Log.e("Rest Response", error.toString());
+            }
+        }){
+            //Override getHeaders() to set Credentials for REST-Authentication
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> headers = new HashMap<>();
+                String credentials = "surfer:hangloose";
+                String auth = "Basic "
+                        + Base64.encodeToString(credentials.getBytes(), Base64.NO_WRAP);
+                headers.put("Content-Type", "application/json");
+                headers.put("Authorization", auth);
+                return headers;
+            }
+        };
+        requestQueue.add(objectRequest);
     }
 }
--- app/src/main/java/com/example/weitblickapp_android/ui/ranking/RankingViewModel.java
+++ app/src/main/java/com/example/weitblickapp_android/ui/ranking/RankingViewModel.java
@@ -6,14 +6,49 @@ import androidx.lifecycle.ViewModel;
 
 public class RankingViewModel extends ViewModel {
 
-    private MutableLiveData<String> mText;
+    private String username;
+    private float km;
+    private float donation;
+    private String profileImage;
 
-    public RankingViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("Hier werden bald Rankings stehen!");
+    public RankingViewModel(){};
+
+    public RankingViewModel(String username, float km, float donation, String imageProfile){
+        this.username = username;
+        this.donation = donation;
+        this.km = km;
+        this.profileImage = imageProfile;
+    };
+
+    public String getUsername() {
+        return username;
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public float getKm() {
+        return km;
+    }
+
+    public void setKm(float km) {
+        this.km = km;
+    }
+
+    public float getDonation() {
+        return donation;
+    }
+
+    public void setDonation(float donation) {
+        this.donation = donation;
+    }
+
+    public String getProfileImage() {
+        return profileImage;
+    }
+
+    public void setProfileImage(String profileImage) {
+        this.profileImage = profileImage;
     }
 }
