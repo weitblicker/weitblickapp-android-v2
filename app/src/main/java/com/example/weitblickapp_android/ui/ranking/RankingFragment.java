41b22977eb3027a1ecceb75aff5a065fecaced8f
jpunke <j.punke@yahoo.de>
2019-12-13 12:55:10 +0100

    onClick for whole Contact, FAQ & Blog

--- app/src/main/java/com/example/weitblickapp_android/ui/ranking/RankingFragment.java
+++ app/src/main/java/com/example/weitblickapp_android/ui/ranking/RankingFragment.java
@@ -112,7 +112,6 @@ public class RankingFragment extends ListFragment{
                     } catch (JSONException e) {
                         e.printStackTrace();
                     }
-
                 }
             }
 
